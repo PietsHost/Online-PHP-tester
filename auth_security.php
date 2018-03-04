47aa7a0acc093d36e247be416e308e1e483cff36
Paul <24229904+paul1278@users.noreply.github.com>
2018-03-04 17:40:47 +0100

    Create auth_security.php

---
+++ auth_security.php
@@ -0,0 +1,25 @@
+<?php
+$my_area = "Online PHP-tester";
+
+//SETUP for user-account(s)
+$login_data = array("admin" => "1234abcd");
+
+
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="' . $my_area . '"');
+	auth_fail();
+} 
+else {
+	if(!isset($login_data[$_SERVER['PHP_AUTH_USER']])) {
+		auth_fail();
+	}
+    if($login_data[$_SERVER['PHP_AUTH_USER']] != $_SERVER['PHP_AUTH_PW']) {
+		auth_fail();
+    }
+}
+function auth_fail() {	
+	header('HTTP/1.0 401 Unauthorized');
+	echo 'Authentification failed.';
+	exit;
+}
+?>
