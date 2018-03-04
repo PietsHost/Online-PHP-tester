b9fd7f1972b2b146cbeedb0fcbdca1ab300d74f8
Paul <24229904+paul1278@users.noreply.github.com>
2018-03-04 17:56:21 +0100

    Added password-encryption
    using password_hash()

--- auth_security.php
+++ auth_security.php
@@ -1,9 +1,10 @@
 <?php
 $my_area = "Online PHP-tester";
 
-//SETUP for user-account(s) "username" => "password"
-$login_data = array("admin" => "1234abcd");
-
+//SETUP for user-account(s) using "username" => password_hash("your_password",PASSWORD_DEFAULT)
+//default user-data is "admin" and "1234abcd"
+//to generate a new hash from the web just call auth_security.php?generate_password=your_password
+$login_data = array("admin" => '$2y$10$cwbYWmbgm6XRiHACioKwQ.NcWsnAtnb0lo2EqJz/qlMm9ClqTIYOC');
 
 if (!isset($_SERVER['PHP_AUTH_USER'])) {
     header('WWW-Authenticate: Basic realm="' . $my_area . '"');
@@ -13,9 +14,12 @@ else {
 	if(!isset($login_data[$_SERVER['PHP_AUTH_USER']])) {
 		auth_fail();
 	}
-    if($login_data[$_SERVER['PHP_AUTH_USER']] != $_SERVER['PHP_AUTH_PW']) {
+    if(!password_verify($_SERVER['PHP_AUTH_PW'],$login_data[$_SERVER['PHP_AUTH_USER']])) {
 		auth_fail();
     }
+	if(isset($_GET["generate_password"])) {
+		exit(password_hash($_GET["generate_password"],PASSWORD_DEFAULT));
+	}
 }
 function auth_fail() {	
 	header('HTTP/1.0 401 Unauthorized');
