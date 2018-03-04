4b7bab5c11bd311132f32539e0e20d513472e4eb
Paul <24229904+paul1278@users.noreply.github.com>
2018-03-04 17:57:02 +0100

    Update auth_security.php

--- auth_security.php
+++ auth_security.php
@@ -7,16 +7,16 @@ $my_area = "Online PHP-tester";
 $login_data = array("admin" => '$2y$10$cwbYWmbgm6XRiHACioKwQ.NcWsnAtnb0lo2EqJz/qlMm9ClqTIYOC');
 
 if (!isset($_SERVER['PHP_AUTH_USER'])) {
-    header('WWW-Authenticate: Basic realm="' . $my_area . '"');
+	header('WWW-Authenticate: Basic realm="' . $my_area . '"');
 	auth_fail();
 } 
 else {
 	if(!isset($login_data[$_SERVER['PHP_AUTH_USER']])) {
 		auth_fail();
 	}
-    if(!password_verify($_SERVER['PHP_AUTH_PW'],$login_data[$_SERVER['PHP_AUTH_USER']])) {
+	if(!password_verify($_SERVER['PHP_AUTH_PW'],$login_data[$_SERVER['PHP_AUTH_USER']])) {
 		auth_fail();
-    }
+    	}
 	if(isset($_GET["generate_password"])) {
 		exit(password_hash($_GET["generate_password"],PASSWORD_DEFAULT));
 	}
